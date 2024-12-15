353404d4274bd01037c2fa6f5758124968a811b5
anouar <anwar.class36flow@gmail.com>
2024-12-15 15:17:29 +0100

    html and css de page dach borde

--- my_php_project/index.php
+++ my_php_project/index.php
@@ -1,18 +1,63 @@
 <?php
-include 'db.php';
-$sql = "SELECT * FROM Person"; 
-$result = $conn->query($sql);
-if ($result->num_rows > 0) {
+// include 'db.php';
+// $sql = "SELECT * FROM Person"; 
+// $result = $conn->query($sql);
+// if ($result->num_rows > 0) {
     
-    while ($row = $result->fetch_assoc()) {
-        echo "ID: " . $row['PersonID'] . " - الاسم: " . $row['FirstName'] . "<br>";
-    }
-} else {
-    echo "not data ";
-}
+//     while ($row = $result->fetch_assoc()) {
+//         echo "ID: " . $row['PersonID'] . " - الاسم: " . $row['FirstName'] . "<br>";
+//     }
+// } else {
+//     echo "not data ";
+// }
 
 
-$conn->close();
+// $conn->close();
+print 'WE LOve PHP';
+print '<br>';
+print 'WE LOve PHP';
+print '<br>';
+echo 5 + (int)"5 anouar";
+print '<br>';
+echo (double)5 + 5.2;
+print '<br>';
+// ===========================================
+var_dump((bool) "");
+print '<br>';
+var_dump((bool) array());
+print '<br>';
+var_dump((bool) []);
+print '<br>';
+print '<br>';
+var_dump((bool) 1);
+print '<br>';
+var_dump((bool) "anouar");
+print '<br>'; 
+var_dump((bool) array(1));
+print '<br>';
+var_dump((bool) [1]);
+print '<br>';
+print '<br>';
+
+
+$name ="anouar";
+echo <<< "Here"
+hallow Anouar 
+special character $$$$ '''' \\\\
+ hallo My Name Is $name 
+Here;
+print '<br>';
+print '<br>';
+
+echo <<<'Now'
+hallow Anouar 
+special character $$$$ '''' \\\\ 
+hallo My Name Is $name 
+
+Now ;
+
+
+
 ?>
 
 
