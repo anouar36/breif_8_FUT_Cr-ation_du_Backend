d5b5345060d0f07a72a3edad38dba1eab7095e35
anouar <anwar.class36flow@gmail.com>
2024-12-17 20:37:13 +0100

    MODIFICATION FOR FORM DE add player

--- my_php_project/from.php
+++ my_php_project/from.php
@@ -4,6 +4,12 @@ if(isset($_post['submit'])){
   $name =$_POST['name'];
   $email =$_POST['email'];
   $adderss =$_POST['address']
+  $query = "INSERT players.name_player, players.photo, clubs.logo, nationality.flag
+FROM players
+INNER JOIN clubs ON players.id_club = clubs.id
+INNER JOIN nationality ON players.id_natio = nationality.id"; 
+$result = mysqli_query($con,$query);
+
 }
 ?>
 
@@ -17,137 +23,6 @@ if(isset($_post['submit'])){
   <link rel="stylesheet" href="/my_php_project/iIndex.css">
 </head>
 <body>
-  <div class="container">
-    <!-- Title section -->
-    <div class="title">Registration</div>
-    <div class="content">
-      <!-- Registration form -->
-      <form method="POSTE" action="#">
-        <div class="user-details">
-          <!-- Input for Full Name -->
-          <div class="input-box">
-            <span class="details">Full Name</span>
-            <input type="text" placeholder="Enter your name" required>
-          </div>
-          <!-- Input for Username -->
-          <div class="input-box">
-            <span class="details">CLUB</span>
-            <select id="position" class="input1" name="cars" id="cars">
-              <option value="Fenerbahçe">Fenerbahçe</option>
-              <option value="Paris Saint-Germain">Paris Saint-Germain</option>
-              <option value="Al-Ittihad">Al-Ittihad</option>
-              <option value="Manchester United">Manchester United</option>
-              <option value="Atletico Madrid">Atletico Madrid</option>
-              <option value="Bayern Munich">Bayern Munich</option>
-              <option value="Al-Hilal">Al-Hilal</option>
-              <option value="Manchester United">Manchester United</option>
-              <option value="Liverpool">Liverpool'</option>
-              <option value="Real Madrid">Real Madrid</option>
-              <option value="Al Nassr">Al Nassr</option>
-              <option value="Inter Miami">Inter Miami</option>
-            </select>
-           
-          </div>
-          <!-- Input for Email -->
-          <div class="input-box">
-            <span class="details">NATIONALETE</span>
-            <div class="input">
-              <select id="position" class="input1" name="cars" id="cars">
-                <option value="Slovenia">Slovenia</option>
-                <option value="Italy">Italy</option>
-                <option value="Italy"></option>
-                <option value="Italy">Norway</option>
-                <option value="Morocco">Morocco</option>
-                <option value="Croatia">Croatia</option>
-                <option value="Egypt">Egypt</option>
-                <option value="Brazil">Brazil</option>
-                <option value="Germany">Germany</option>
-                <option value="Netherlands">Netherlands</option>
-                <option value="France">France</option>
-                <option value="Belgium">Belgium</option>
-                <option value="Portugal">Portugal</option>
-                <option value="Argentina">Argentina</option>
-              </select>
-          </div>
-        </div>
-          <!-- Input for Phone Number -->
-          <div class="input-box">
-            <span class="details">POSION</span>
-            <div class="input">
-              <select id="position" class="input1" name="cars" id="cars">
-                <option value="CF">CF</option>
-                <option value="LWF">LWF</option>
-                <option value="RWF">RWF</option>
-                <option value="CM2">CM2</option>
-                <option value="CM1">CM1</option>
-                <option value="DM">DMF</option>
-                <option value="CB1">CB1</option>
-                <option value="CB2">CB2</option>
-                <option value="RB">RB</option>
-                <option value="LB">LB</option>
-                <option value="GK">GK</option>
-              </select>
-            </div>
-          </div>
-          <!-- Input for Password -->
-          <div class="input-box">
-            <span class="details">Password</span>
-            <input type="text" placeholder="Enter your password" required>
-          </div>
-          <!-- Input for Confirm Password -->
-          <div class="input-box">
-            <span class="details">Confirm Password</span>
-            <input type="text" placeholder="Confirm your password" required>
-          </div>
-          <!-- Input for Confirm Password -->
-          <div class="input-box">
-            <span class="details">Confirm Password</span>
-            <input type="text" placeholder="Confirm your password" required>
-          </div>
-          <!-- Input for Confirm Password -->
-          <div class="input-box">
-            <span class="details">Confirm Password</span>
-            <input type="text" placeholder="Confirm your password" required>
-          </div>
-          <!-- Input for Confirm Password -->
-          <div class="input-box">
-            <span class="details">Confirm Password</span>
-            <input type="text" placeholder="Confirm your password" required>
-          </div>
-          <!-- Input for Confirm Password -->
-          <div class="input-box">
-            <span class="details">Confirm Password</span>
-            <input type="text" placeholder="Confirm your password" required>
-          </div>
-          <!-- Input for Confirm Password -->
-          <div class="input-box">
-            <span class="details">Confirm Password</span>
-            <input type="text" placeholder="Confirm your password" required>
-          </div>
-          <!-- Input for Confirm Password -->
-          <div class="input-box">
-            <span class="details">Confirm Password</span>
-            <input type="text" placeholder="Confirm your password" required>
-          </div>
-          <!-- Input for Confirm Password -->
-          <div class="input-box">
-            <span class="details">Confirm Password</span>
-            <input type="text" placeholder="Confirm your password" required>
-          </div>
-          <!-- Input for Confirm Password -->
-          <div class="input-box">
-            <span class="details">Confirm Password</span>
-            <input type="text" placeholder="Confirm your password" required>
-          </div>
-
-        </div>
-       
-        <!-- Submit button -->
-        <div class="button">
-          <input type="submit" value="Register">
-        </div>
-      </form>
-    </div>
-  </div>
+  
 </body>
 </html>
--- my_php_project/index.php
+++ my_php_project/index.php
@@ -15,10 +15,142 @@ $result = mysqli_query($con,$query);
  	<!-- Boxicons -->
  	<link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
  	<!-- My CSS -->
- 	<link rel="stylesheet" href="index.css">
+ 	<link rel="stylesheet" href="style.css">
  	<title>AdminHub</title>
  </head>
  <body>
+ <div class="container">
+    <!-- Title section -->
+    <div class="title">Registration</div>
+    <div class="content">
+      <!-- Registration form -->
+      <form method="POSTE" action="#">
+        <div class="user-details">
+          <!-- Input for Full Name -->
+          <div class="input-box">
+            <span class="details">Full Name</span>
+            <input type="text" placeholder="Enter your name" required>
+          </div>
+          <!-- Input for Username -->
+          <div class="input-box">
+            <span class="details">CLUB</span>
+            <select id="position" class="input1" name="cars" id="cars">
+              <option value="Fenerbahçe">Fenerbahçe</option>
+              <option value="Paris Saint-Germain">Paris Saint-Germain</option>
+              <option value="Al-Ittihad">Al-Ittihad</option>
+              <option value="Manchester United">Manchester United</option>
+              <option value="Atletico Madrid">Atletico Madrid</option>
+              <option value="Bayern Munich">Bayern Munich</option>
+              <option value="Al-Hilal">Al-Hilal</option>
+              <option value="Manchester United">Manchester United</option>
+              <option value="Liverpool">Liverpool'</option>
+              <option value="Real Madrid">Real Madrid</option>
+              <option value="Al Nassr">Al Nassr</option>
+              <option value="Inter Miami">Inter Miami</option>
+            </select>
+           
+          </div>
+          <!-- Input for Email -->
+          <div class="input-box">
+            <span class="details">NATIONALETE</span>
+            <div class="input">
+              <select id="position" class="input1" name="cars" id="cars">
+                <option value="Slovenia">Slovenia</option>
+                <option value="Italy">Italy</option>
+                <option value="Italy"></option>
+                <option value="Italy">Norway</option>
+                <option value="Morocco">Morocco</option>
+                <option value="Croatia">Croatia</option>
+                <option value="Egypt">Egypt</option>
+                <option value="Brazil">Brazil</option>
+                <option value="Germany">Germany</option>
+                <option value="Netherlands">Netherlands</option>
+                <option value="France">France</option>
+                <option value="Belgium">Belgium</option>
+                <option value="Portugal">Portugal</option>
+                <option value="Argentina">Argentina</option>
+              </select>
+          </div>
+        </div>
+          <!-- Input for Phone Number -->
+          <div class="input-box">
+            <span class="details">POSION</span>
+            <div class="input">
+              <select id="position" class="input1" name="cars" id="cars">
+                <option value="CF">CF</option>
+                <option value="LWF">LWF</option>
+                <option value="RWF">RWF</option>
+                <option value="CM2">CM2</option>
+                <option value="CM1">CM1</option>
+                <option value="DM">DMF</option>
+                <option value="CB1">CB1</option>
+                <option value="CB2">CB2</option>
+                <option value="RB">RB</option>
+                <option value="LB">LB</option>
+                <option value="GK">GK</option>
+              </select>
+            </div>
+          </div>
+          <!-- Input for Password -->
+          <div class="input-box">
+            <span class="details">Password</span>
+            <input type="text" placeholder="Enter your password" required>
+          </div>
+          <!-- Input for Confirm Password -->
+          <div class="input-box">
+            <span class="details">Confirm Password</span>
+            <input type="text" placeholder="Confirm your password" required>
+          </div>
+          <!-- Input for Confirm Password -->
+          <div class="input-box">
+            <span class="details">Confirm Password</span>
+            <input type="text" placeholder="Confirm your password" required>
+          </div>
+          <!-- Input for Confirm Password -->
+          <div class="input-box">
+            <span class="details">Confirm Password</span>
+            <input type="text" placeholder="Confirm your password" required>
+          </div>
+          <!-- Input for Confirm Password -->
+          <div class="input-box">
+            <span class="details">Confirm Password</span>
+            <input type="text" placeholder="Confirm your password" required>
+          </div>
+          <!-- Input for Confirm Password -->
+          <div class="input-box">
+            <span class="details">Confirm Password</span>
+            <input type="text" placeholder="Confirm your password" required>
+          </div>
+          <!-- Input for Confirm Password -->
+          <div class="input-box">
+            <span class="details">Confirm Password</span>
+            <input type="text" placeholder="Confirm your password" required>
+          </div>
+          <!-- Input for Confirm Password -->
+          <div class="input-box">
+            <span class="details">Confirm Password</span>
+            <input type="text" placeholder="Confirm your password" required>
+          </div>
+          <!-- Input for Confirm Password -->
+          <div class="input-box">
+            <span class="details">Confirm Password</span>
+            <input type="text" placeholder="Confirm your password" required>
+          </div>
+          <!-- Input for Confirm Password -->
+          <div class="input-box">
+            <span class="details">Confirm Password</span>
+            <input type="text" placeholder="Confirm your password" required>
+          </div>
+
+        </div>
+       
+        <!-- Submit button -->
+        <div class="button">
+          <input type="submit" value="Register">
+        </div>
+      </form>
+    </div>
+  </div>
 
  	<!-- SIDEBAR -->
  	<section id="sidebar">
